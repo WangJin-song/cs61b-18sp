f240c415cb74ce9190eea0f654a142486c478c2e
Marcus Wang <kawhiwang2@gmail.com>
2022-06-14 15:39:51 +0800

    completed proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -21,9 +21,9 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
         assertTrue(offByOne.equalChars('&', '%'));
-        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'B'));
         assertFalse(offByOne.equalChars('a', 'z'));
         assertFalse(offByOne.equalChars('a', 'e'));
-        assertFalse(offByOne.equalChars('a', 'A'));
+        assertFalse(offByOne.equalChars('A', 'b'));
     }
 }
